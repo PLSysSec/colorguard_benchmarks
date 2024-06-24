a71eaefe8c69a1f821cc65460445e3ba93549412
Evan Johnson <e5johnso@ucsd.edu>
2024-06-23 20:05:36 -0500

    update benchmark

--- examples/load_balancing/src/main.rs
+++ examples/load_balancing/src/main.rs
@@ -13,20 +13,27 @@ fn delay() {
   // let t = delay_time();
   // let ten_millis = time::Duration::from_millis(10);
   // sleep(ten_millis);
-  let lambda = 5.0;
+  let lambda = 10.0;
   let poi = Poisson::new(lambda).unwrap();
   let v = poi.sample(&mut rand::thread_rng());
+  //println!("v = {:?} {:?}", v, v as u64);
   let millis = time::Duration::from_millis(v as u64);
   sleep(millis);
 }
 
 fn main() {
 
-  delay();
+  //delay();
 
   let mut rng = thread_rng();
   let n: u16 = rng.gen();
-  // println!("{:x}", n);
+  //let num_iterations = 10;
+  // let chosen = rng.gen::<usize>() % num_iterations;
+  //for idx in 0..num_iterations {
+  //if idx == chosen {
+    delay();
+  //}
+      // println!("{:x}", n);
   //Initialize hashring
   let mut ring: HashRing <&str> = HashRing::new();
   ring.add("origin_0");
@@ -34,12 +41,15 @@ fn main() {
   ring.add("origin_2");
   //use query to get hash
   let query = format!("/page/{n}");
-  println!("query: {}", query);
+  //println!("query: {}", query);
 
 //   let query = req.get_query_str().unwrap_or("string");
   let backend = ring.get(&query).unwrap();
 
+
+  delay();
   //debugging...
-  println!("Sending {n} to backend {backend} ...");  
+  //println!("Sending {n} to backend {backend} ...");  
   // println!("{:x}", n);
+  //}
 }
