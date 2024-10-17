ada97b0a404cac9d3da5dd01e8f35e4d111c0619
Anjo Vahldiek-Oberwagner <anjovahldiek@gmail.com>
2024-10-17 04:36:28 -0700

    adding dstat context switch and pagefault data collection

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ Maybe necessary:
 # Known issue
 
 * Harness requires rust 1.76.0 while examples are build for 1.73.0 (like the remaining segue/colorguard repo). May need to change defaults in between.
+
+# Recording context switches and page faults
+
+We use `dstat --sys --cpu-adv -ipc --lock --vm --noheaders` to record the context switches and page faults.
--- harness/run_no_mpk_parallel.sh
+++ harness/run_no_mpk_parallel.sh
@@ -1,48 +1,63 @@
 mkdir -p out
 
-rm out/no_mpk_parallel_1.txt
+rm -f out/no_mpk_parallel_1.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_1_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_1.txt
+pkill dstat
 
-rm out/no_mpk_parallel_2.txt
+rm -f out/no_mpk_parallel_2.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_2_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_2.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_2.txt
+pkill dstat
 
-rm out/no_mpk_parallel_3.txt
+rm -f out/no_mpk_parallel_3.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_3_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_3.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_3.txt &
-taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_3.txt 
+taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_3.txt
+pkill dstat
 
-rm out/no_mpk_parallel_4.txt
+rm -f out/no_mpk_parallel_4.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_4_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_4.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_4.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_4.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_4.txt
+pkill dstat
 
-rm out/no_mpk_parallel_5.txt
+rm -f out/no_mpk_parallel_5.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_5_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_5.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_5.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_5.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_5.txt &
-taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_5.txt 
+taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_5.txt
+pkill dstat
 
-rm out/no_mpk_parallel_6.txt
+rm -f out/no_mpk_parallel_6.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_6_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_6.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_6.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_6.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_6.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_6.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_6.txt
+pkill dstat
 
-rm out/no_mpk_parallel_7.txt
+rm -f out/no_mpk_parallel_7.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_7_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_7.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_7.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_7.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_7.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_7.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_7.txt &
-taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_7.txt 
+taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_7.txt
+pkill dstat
 
-rm out/no_mpk_parallel_8.txt
+rm -f out/no_mpk_parallel_8.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_8_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_8.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_8.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_8.txt &
@@ -51,8 +66,10 @@ taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_para
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_8.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_8.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_8.txt
+pkill dstat
 
-rm out/no_mpk_parallel_9.txt
+rm -f out/no_mpk_parallel_9.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_9_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_9.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_9.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_9.txt &
@@ -61,9 +78,11 @@ taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_para
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_9.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_9.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_9.txt &
-taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_9.txt 
+taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_9.txt
+pkill dstat
 
-rm out/no_mpk_parallel_10.txt
+rm -f out/no_mpk_parallel_10.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_10_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_10.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_10.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_10.txt &
@@ -74,8 +93,10 @@ taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_para
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_10.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_10.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_10.txt
+pkill dstat
 
-rm out/no_mpk_parallel_11.txt
+rm -f out/no_mpk_parallel_11.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_11_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_11.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_11.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_11.txt &
@@ -86,10 +107,12 @@ taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_para
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_11.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_11.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_11.txt &
-taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_11.txt 
+taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_11.txt
+pkill dstat
 
 
-rm out/no_mpk_parallel_12.txt
+rm -f out/no_mpk_parallel_12.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_12_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_12.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_12.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_12.txt &
@@ -102,9 +125,11 @@ taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_para
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_12.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_12.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_12.txt
+pkill dstat
 
 
-rm out/no_mpk_parallel_13.txt
+rm -f out/no_mpk_parallel_13.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_13_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_13.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_13.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_13.txt &
@@ -118,9 +143,11 @@ taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_para
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_13.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_13.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_13.txt
+pkill dstat
 
 
-rm out/no_mpk_parallel_14.txt
+rm -f out/no_mpk_parallel_14.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_14_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_14.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_14.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_14.txt &
@@ -135,9 +162,11 @@ taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_para
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_14.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_14.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_14.txt
+pkill dstat
 
 
-rm out/no_mpk_parallel_15.txt
+rm -f out/no_mpk_parallel_15.txt
+dstat -C 2 --sys --cpu-adv -ipc --lock --vm --noheaders >  out/no_mpk_parallel_15_dstat.csv &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_15.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_15.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_15.txt &
@@ -153,6 +182,7 @@ taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_para
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_15.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_15.txt &
 taskset -c 2 target/release/harness $1  14000 1000 no | tee -a ./out/no_mpk_parallel_15.txt
+pkill dstat
 
 
 
